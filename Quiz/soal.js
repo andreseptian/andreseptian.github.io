3c066d09a779e7aa18394e20327dceb607e5c604
andreseptian <andre.septian2016@student.uny.ac.id>
2017-06-21 14:58:54 +0700

    Add files via upload

---
+++ Quiz/soal.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "5 x 10 = ....",
+	"option1" : "30",
+	"option2" : "50",
+	"option3" : "40",
+	"option4" : "60",
+	"answer":"2"
+}, {
+	"question": "Lambang bilangan negatif dua ratus dua belas adalah ...",
+	"option1" : "122",
+	"option2" : "-122",
+	"option3" : "-212",
+	"option4" : "212",
+	"answer":"3"
+}, {
+	"question": "10 - (-6) = ....",
+	"option1" : "16",
+	"option2" : "12",
+	"option3" : "4",
+	"option4" : "-4",
+	"answer":"1"
+}, {
+		"question": "-11, -7, -12, 10, 0 Urutan yang benar dari bilangan terkecil adalah ....",
+	"option1" : "-11, -12, -7, 10, 0",
+	"option2" : "-12, -11, -7, 0, 10",
+	"option3" : "0, 10, -7, -11, -12",
+	"option4" : "-12, -11, 0, -7, 10",
+	"answer":"2"
+}, {
+		"question": "-9 + (-12) = n. Nilai n = . . . .",
+	"option1" : "3",
+	"option2" : "-3",
+	"option3" : "-21",
+	"option4" : "21",
+	"answer":"3"
+}, {
+	"question": "Diketahui -400 - c = 200, maka nilai c = . . . .",
+	"option1" : "-600",
+	"option2" : "-200",
+	"option3" : "600",
+	"option4" : "200",
+	"answer":"1"
+}, {
+	"question": "Roni sedang mendaki gunung. Ia berada pada ketinggian 185 m di atas permukaan laut. Kemudian ia naik lagi sejauh 125 m. Roni sekarang berada pada ketinggian ....",
+	"option1" : "60 meter",
+	"option2" : "210 meter",
+	"option3" : "310 meter",
+	"option4" : "410 meter",
+	"answer":"3"
+}, {
+		"question": "-18 - (-8) = ....",
+	"option1" : "-8",
+	"option2" : "-10",
+	"option3" : "-12",
+	"option4" : "-15",
+	"answer":"2"
+}, {
+		"question": "-7 - 8 = ....",
+	"option1" : "-8",
+	"option2" : "-10",
+	"option3" : "-12",
+	"option4" : "-15",
+	"answer":"4"
+}, {
+		"question": "-16 ... -25 , Lambang perbandingan yang tepat adalah ....",
+	"option1" : "<",
+	"option2" : "=",
+	"option3" : ">",
+	"option4" : ":",
+	"answer":"3"
+}]
